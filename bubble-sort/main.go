ef19dd18ada9e57d85a3d3364aae396fa8cb46db
sidz111 <sssurwade2212@gmail.com>
2026-02-11 19:14:43 +0530

    Bubble-Sort

---
+++ bubble-sort/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{64, 34, 25, 12, 22, 11, 90}
+
+	n := len(arr)
+
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+
+	fmt.Println("Sorted array:", arr)
+}
