a21d178d68943032a31b5d16ac65b58c1f476f65
sidz111 <sssurwade2212@gmail.com>
2026-02-11 19:11:42 +0530

    LinkedList

--- LinkedList/main.go
+++ LinkedList/main.go
@@ -2,6 +2,33 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("Hello World")
+type Node struct {
+	data int
+	next *Node
+}
+
+// print list
+func printList(head *Node) {
+	temp := head
+	for temp != nil {
+		fmt.Print(temp.data, " -> ")
+		temp = temp.next
+	}
+	fmt.Println("nil")
+}
+
+func main() {
+
+	// nodes create karu
+	n1 := &Node{data: 10}
+	n2 := &Node{data: 20}
+	n3 := &Node{data: 30}
+
+	// connect karu
+	n1.next = n2
+	n2.next = n3
+
+	head := n1
+
+	printList(head)
 }
