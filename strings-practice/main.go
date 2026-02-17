97a3183f61a379cfacc5b2d0f7fe1de5c4720137
sidz111 <sssurwade2212@gmail.com>
2026-02-17 12:10:19 +0530

    Strings Methods

--- strings-practice/main.go
+++ strings-practice/main.go
@@ -1,17 +1,29 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	str := "Hello"
+	// str := "Hello"
 
 	//length of String
-	fmt.Println("len is", len(str))
+	// fmt.Println("len is", len(str))
 
 	//Reversed String
-	for i := len(str) - 1; i >= 0; i-- {
-		fmt.Print(string(str[i]))
-	}
+	// for i := len(str) - 1; i >= 0; i-- {
+	// 	fmt.Print(string(str[i]))
+	// }
+
+	// str := "asdsa"
+
+	// newStr := ""
+
+	// for i := len(str) - 1; i >= 0; i-- {
+	// 	newStr += string(str[i])
+	// }
+	// fmt.Println("str is", str)
+	// fmt.Println("new Str is", newStr)
+	// fmt.Println("is Palindrome", str == newStr)
+
+	// s := "Siddhu Surwade"
+	// fmt.Println(strings.Trim(s, " "))
+	// fmt.Println(strings.ToLower(strings.ReplaceAll(s, " ", "")))
+
 }
