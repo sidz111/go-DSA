0236fb0f958f2efff660af0ff87b87663eb29723
sidz111 <sssurwade2212@gmail.com>
2026-03-02 18:20:23 +0530

    add insert at beginning

--- LinkedList/practice/main.go
+++ LinkedList/practice/main.go
@@ -8,7 +8,7 @@ type Node struct {
 }
 
 func Show(head *Node) {
-	temp := head //[data,next] = [4, address of node2]
+	temp := head
 	for temp != nil {
 		fmt.Print(temp.data, " -> ")
 		temp = temp.next
@@ -16,15 +16,28 @@ func Show(head *Node) {
 	fmt.Println("nil")
 }
 
+//func for Insert at beginning
+func InsertAtBeginning(head *Node, data int) *Node {
+	newNode := &Node{data: data}
+	newNode.next = head
+	return newNode
+}
+
 func main() {
 
-	node1 := &Node{data: 4}
-	node2 := &Node{data: 9}
-	node3 := &Node{data: 40}
-
 	var head *Node
+	node1 := &Node{data: 30}
+	node2 := &Node{data: 40}
+	node3 := &Node{data: 50}
+
 	head = node1
 	node1.next = node2
 	node2.next = node3
+
+	newNode := &Node{data: 20}
+	newNode.next = head
+	head = newNode
+	head = InsertAtBeginning(head, 2)
 	Show(head)
+
 }
